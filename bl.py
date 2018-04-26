c26851e30865fd41337e32721a29411ee152f70e
Igor Kasianov <super.hang.glider@gmail.com>
2018-04-26 16:21:53 +0300

    1

--- bl.py
+++ bl.py
@@ -1,5 +1,6 @@
 import elasticsearch as es
 from elasticsearch.client import IndicesClient
+from elasticsearch.helpers import bulk
 from elasticsearch_dsl import Search
 
 from settings import INDEX_NAME
@@ -9,3 +10,32 @@ index_client = IndicesClient(client)
 
 def get_search(index_name=INDEX_NAME):
     return Search(using=client).index(*[index_name])
+
+def index_tutors(tutors, **bulk_kwargs):
+    """
+
+    :param tutors: lit of dicts
+    :return:
+    """
+    res = []
+    for tutor in tutors:
+        request = {
+            '_index': INDEX_NAME,
+            '_type': INDEX_NAME,
+            '_id': tutor['id'],
+            '_op_type': 'index',
+            '_source': tutor,
+        }
+        res.append(request)
+    bulk(client, res, **bulk_kwargs)
+
+
+
+
+def find_tutors():
+    """
+
+    :return:
+    """
+    res = get_search().execute()
+    return [tutor.id for tutor in res.hits]
--- tests.py
+++ tests.py
@@ -1,5 +1,16 @@
-from bl import get_search
+from bl import get_search, index_tutors, find_tutors
 
 
 def test_basic():
     assert get_search().count() == 0
+
+def test_get_all_tutors():
+    index_tutors([
+        {
+            'id': 1
+        },
+        {
+            'id': 2
+        }
+    ], refresh=True)
+    assert {1, 2} == set(find_tutors())
